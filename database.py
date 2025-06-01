d634332f5962032b67c4ea045bb0e770e9dc52bd
Maga <mcmagakrytoy@gmail.com>
2025-06-01 13:55:48 +0300

    добавил класс Database и Client

--- database.py
+++ database.py
@@ -1,70 +1,49 @@
 import mysql.connector
-from mysql.connector import Error
 
-# Функция для создания соединения с MySQL
-def create_connection(host_name, user_name, user_password, db_name=None):
-    """Создает соединение с MySQL базой данных"""
-    conn = None
-    try:
-        conn = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            password=user_password,
-            database=db_name
-        )
-        if conn.is_connected():
-            print("Успешное подключение к базе данных")
-    except Error as e:
-        print(f"Ошибка '{e}' при подключении к базе данных")
 
-    return conn
+connection = mysql.connector.connect(
+    user='seschool_01',
+    password='seschool_01',
+    host='109.206.169.221',
+    database='seschool_01_pks1'
+)
 
-# Функция для создания базы данных
-def create_database(connection, db_name):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
-        print(f"База данных '{db_name}' успешно создана")
-    except Error as e:
-        print(f"Ошибка '{e}' при создании базы данных")
-
-# Функция для создания таблицы клиентов
-def create_clients_table(connection):
-    cursor = connection.cursor()
-    sql_create_clients_table = """
-    CREATE TABLE IF NOT EXISTS clients (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        date_training DATETIME NOT NULL,
-        time_training INT,
-        type_training VARCHAR(15) NOT NULL,
-
-    );
-    """
-    cursor.execute(sql_create_clients_table)
-    print("Таблица 'clients' успешно создана")
-
-# Функция для добавления нового клиента
-def add_client(connection, name, date_training, time_training, type_training):
-    """Добавляет нового клиента в таблицу клиентов"""
-    sql = '''INSERT INTO clients(name, date_training, time_training, type_training)
-              VALUES(%s, %s, %s, %s)'''
-    cursor = connection.cursor()
-    cursor.execute(sql, (name, date_training, time_training, type_training))
-    connection.commit()
-    print("Клиент успешно добавлен")
-    return cursor.lastrowid
-
-# Пример использования
-if __name__ == '__main__':
-    conn = create_connection('localhost', 'your_username', 'your_password')
+class Database: 
+    __conection = None
     
-    # Создаем базу данных
-    create_database(conn, 'gym_bot')
-    
-    # Подключаемся к только что созданной базе данных
-    conn.close()
-    conn = create_connection('localhost', 'your_username', 'your_password', 'gym_bot')
+    @classmethod
+    def open(cls, 
+             host='109.206.169.221', 
+             user='seschool_01', 
+             password='seschool_01', 
+             database='seschool_01_pks1'):
+        if cls.__conection is None:
+            cls.__conection = mysql.connector.connect(
+                user=user,
+                password=password,
+                host=host,
+                database=database   
+            )
 
-    # Создаем таблицу клиентов
-    create_clients_table(conn)
+
+    @classmethod
+    def query(cls, sql, values):
+        cursor = cls.cls.__conection.cursor()
+        
+        cursor.execute(sql, values)
+        result = cls.__conection.cursor().fetchall()
+        cls.__conection.commit()
+        
+        return result
+    
+    @classmethod
+    def close(cls):
+        cls.__conection.close()
+
+
+class Client:
+    @classmethod
+    def add(cls,client: Client):
+        sql = "INSERT INTO Client (`name`,`date_training`,`time_training`,`type_training`)  VALUE (%s, %s, %s, %s)"
+        values = (client.name, client.date_training, client.time_training, client.type_training)
+        Database.query(sql,values)
