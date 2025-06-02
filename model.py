2f91d176f4afef8f24ad8e977df74232b468eda1
Maga <mcmagakrytoy@gmail.com>
2025-06-02 19:21:08 +0300

    создал новый класс Training, добавил ему поля

--- model.py
+++ model.py
@@ -1,8 +1,13 @@
 class Client:
     id: int
     name: str
-    date_training: str
-    time_training: int
+    phone_number: str
+    
+
+
+class Training:
+    date_training: str #datetime
+    time_training: str #datetime
     type_training: str
     
 
